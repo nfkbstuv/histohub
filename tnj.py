c3ffd27ee83e7c38b00947335c32682b0688c5e3
nfkbstuv <90775354+nfkbstuv@users.noreply.github.com>
2021-09-29 18:05:09 +0300

    Add files via upload

--- tnj.py
+++ tnj.py
@@ -1,16 +1,16 @@
 import math
 
-import pygame as pg
-from pg.draw import *
+import pygame
+from pygame.draw import *
 
-pg.init()
+pygame.init()
 
 FPS = 30
-s = pg.display.set_mode((794, 1123))
+s = pygame.display.set_mode((794, 1123))
 blue = (125, 210, 228)
 green = (111, 192, 98)
 ochre = (201, 172, 58)
-sy = (226, 199, 90)  # stands for selective yellow
+sy = (226, 199, 90)
 gray = (100, 98, 90)
 brown = (107, 92, 67)
 white = (242, 232, 216)
@@ -18,15 +18,15 @@ black = (44, 38, 38)
 
 
 def main():
- draw_tnj()  # whole picture constructor
+ draw_tnj()
  
  
 def draw_tnj():
- bg()  # stands for background
- f()  # stands for fence
- g()  # stands for grass
- dh()  # stands for doghouse
- d()  # stands for dog
+ bg()
+ f()
+ g()
+ dh()
+ d()
 
 
 def bg():
@@ -51,68 +51,80 @@ def g():
  
 def dh():
  dhdraw()
- def dhdraw():
-  roof()
-  sq()
-  ch()
-  def roof():
-   polygon(s, sy, [500, 500], [570, 480], [550, 550])
-   aalines(s, black, True, [500, 500], [570, 250], [550, 550])
-   polygon(s, sy, [550, 550], [570, 480], [600, 500], [560, 570])
-   aalines(s, black, True, [550, 550], [570, 480], [600, 500], [560, 570])
-  def sq():
-   polygon(s, sy, [500, 500], [500, 300], [570, 250], [570, 480])
-   aalines(s, black, True, [500, 500], [500, 300], [570, 250], [570, 480])
-   polygon(s, sy, [570, 480], [570, 250], [600, 310], [600, 500])
-   aalines(s, black, True, [570, 480], [570, 250], [600, 310], [600, 500])
-   circle(s, black, (540, 400), 40)
-  def ch():
-   arc(s, gray, (520, 355, 20, 10), 0, 2*math.pi)
-   arc(s, gray, (515, 350, 10, 20), 0, 2*math.pi)
-   arc(s, gray, (510, 335, 10, 20), 0, 2*math.pi)
-   arc(s, gray, (505, 335, 20, 10), 0, 2*math.pi)
-   arc(s, gray, (490, 330, 20, 10), 0, 2*math.pi)
-   arc(s, gray, (480, 325, 20, 10), 0, 2*math.pi)
-   arc(s, gray, (470, 325, 20, 10), 0, 2*math.pi)
+
+def dhdraw():
+ roof()
+ sq()
+ ch()
+
+def roof():
+ polygon(s, sy, [500, 500], [570, 480], [550, 550])
+ aalines(s, black, True, [500, 500], [570, 250], [550, 550])
+ polygon(s, sy, [550, 550], [570, 480], [600, 500], [560, 570])
+ aalines(s, black, True, [550, 550], [570, 480], [600, 500], [560, 570])
+
+def sq():
+ polygon(s, sy, [500, 500], [500, 300], [570, 250], [570, 480])
+ aalines(s, black, True, [500, 500], [500, 300], [570, 250], [570, 480])
+ polygon(s, sy, [570, 480], [570, 250], [600, 310], [600, 500])
+ aalines(s, black, True, [570, 480], [570, 250], [600, 310], [600, 500])
+ circle(s, black, (540, 400), 40)
+
+def ch():
+ arc(s, gray, (520, 355, 20, 10), 0, 2*math.pi)
+ arc(s, gray, (515, 350, 10, 20), 0, 2*math.pi)
+ arc(s, gray, (510, 335, 10, 20), 0, 2*math.pi)
+ arc(s, gray, (505, 335, 20, 10), 0, 2*math.pi)
+ arc(s, gray, (490, 330, 20, 10), 0, 2*math.pi)
+ arc(s, gray, (480, 325, 20, 10), 0, 2*math.pi)
+ arc(s, gray, (470, 325, 20, 10), 0, 2*math.pi)
   
 
 def d():
  ddraw()
- def ddraw():
-  body()
-  head()
-  def body():
-   corpus()
-   legs()
-   def corpus():
-    pass
-   def legs():
-    pass
-  def head():
-   facenears()
-   eyes()
-   mouth()
-   def facenears():
-    pass
-   def eyes():
-    pass
-   def mouth():
-    pass
+
+def ddraw():
+ body()
+ head()
+
+def body():
+ corpus()
+ legs()
+
+def corpus():
+ pass
+
+def legs():
+ pass
+
+def head():
+ facenears()
+ eyes()
+ mouth()
+
+def facenears():
+ pass
+
+def eyes():
+ pass
+
+def mouth():
+ pass
 
 
 main()
 
-pg.display.update()
+pygame.display.update()
 
-clock = pg.time.Clock()
+clock = pygame.time.Clock()
 finished = False
 while not finished:
     clock.tick(FPS)
-    for event in pg.event.get():
+    for event in pygame.event.get():
         if event.type == pg.QUIT:
             finished = True
 
-pg.quit()
+pygame.quit()
 
  
  
