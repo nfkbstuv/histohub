f8660678c3c5f431535e407965766d4dee7fb247
nfkbstuv <90775354+nfkbstuv@users.noreply.github.com>
2021-10-01 16:17:34 +0300

    Add files via upload

--- tnj.py
+++ tnj.py
@@ -175,7 +175,7 @@ finished = False
 while not finished:
     clock.tick(FPS)
     for event in pygame.event.get():
-        if event.type == pg.QUIT:
+        if event.type == pygame.QUIT:
             finished = True
 
 pygame.quit()
