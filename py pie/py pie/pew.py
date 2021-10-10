58297a1985debcd6d4c4e90d1d48626c691dec3e
nfkbstuv <90775354+nfkbstuv@users.noreply.github.com>
2021-10-10 23:00:10 +0300

    Правильные шарики с бомбами (статика)

--- py pie/py pie/pew.py
+++ py pie/py pie/pew.py
@@ -18,13 +18,13 @@ def new_ball():
    х, у - координаты центра шарика, r - радиус шарика
    
    """
-    global x, y, r, ball, cc
-    x = rnd(100, 700)
-    y = rnd(100, 500)
-    r = rnd(30, 50)
-    cc = choice(colors)
-    ball = canv.create_oval(x-r, y-r, x+r, y+r, fill=cc, width=0)
-    root.after(2500, new_ball)
+   global x, y, r, ball, cc
+   x = rnd(100, 700)
+   y = rnd(100, 500)
+   r = rnd(30, 50)
+   cc = choice(colors)
+   ball = canv.create_oval(x-r, y-r, x+r, y+r, fill=cc, width=0)
+   root.after(2500, new_ball)
 
 
 def click(event):
@@ -32,18 +32,18 @@ def click(event):
    Шарики black - бомбы, которые обнуляют счёт points и удаляяют все предыдущие шарики с холста
    
    """
-    global points, x, tekst
-    if (event.y - y)**2 + (event.x - x)**2 < r**2:
-        if cc == 'black':
-            points = 0
-            x = -1000
-            canv.delete(ALL)
-        else:
-            points += 1
-            x = -1000
-            canv.delete(tekst)
-            canv.delete(ball)
-        tekst = canv.create_text(20, 20, text=str(points), font='Helvetica 36')   # Текст, отображающий текущие points
+   global points, x, tekst
+   if (event.y - y)**2 + (event.x - x)**2 < r**2:
+       if cc == 'black':
+           points = 0
+           x = -1000
+           canv.delete(ALL)
+       else:
+           points += 1
+           x = -1000
+           canv.delete(tekst)
+           canv.delete(ball)
+       tekst = canv.create_text(20, 20, text=str(points), font='Helvetica 36')   # Текст, отображающий текущие points
 
 
 tekst = canv.create_text(20, 20, text=0, font='Helvetica 36')
